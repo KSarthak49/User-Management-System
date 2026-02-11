ad37138097b55f5c921c3f3d5b02c1bd4407e0cc
Sarthak Srivastava <sarthaksrivastava7645@gmail.com>
2026-02-11 21:40:01 +0530

    Bug fixing 2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAuth } from "../context/authContext";
+import { useAuth } from "../context/AuthContext";
 import { useNavigate, Link } from "react-router-dom";
 
 import bgImage from '../assets/background.jpg';
